8015f8845a0fae29dc7c56311b24215b5e733dc8
David Ho <r1244460xx@yahoo.com.tw>
2025-04-01 01:44:26 +0800

    add logger

--- src/main/java/com/symbol/BatteryServer/controller/BatteryController.java
+++ src/main/java/com/symbol/BatteryServer/controller/BatteryController.java
@@ -2,6 +2,8 @@ package com.symbol.BatteryServer.controller;
 
 import com.symbol.BatteryServer.model.Battery;
 import com.symbol.BatteryServer.service.BatteryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.List;
 public class BatteryController {
 
     private final BatteryService batteryService;
+
+    private final static Logger logger = LoggerFactory.getLogger(BatteryController.class);
 
     public BatteryController(BatteryService batteryService) {
         this.batteryService = batteryService;
@@ -29,6 +33,7 @@ public class BatteryController {
         if (count < 1) {
             throw new IllegalArgumentException("Passed count: " + count + " cannot be smaller than 0");
         }
+        logger.info("Battery data pulled");
         return batteryService.pull(count);
     }
 
